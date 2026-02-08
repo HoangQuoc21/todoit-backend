0704e580c4183708bfa6d30b7bc4d7852b824896
Hoàng Quốc <hoangquoc2106@gmail.com>
2026-02-08 22:55:38 +0700

    tạo các module nhỏ

--- backend/expressjs/src/index.ts
+++ backend/expressjs/src/index.ts
@@ -1,6 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import { middlewares } from "./utils";
+import {
+  categoryRouter,
+  notificationRouter,
+  todoRouter,
+  userRouter,
+} from "./routes";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -8,6 +14,11 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.get("/", middlewares.rootHandler);
+
+app.use("/category", categoryRouter);
+app.use("/notification", notificationRouter);
+app.use("/todo", todoRouter);
+app.use("/user", userRouter);
 
 app.use(middlewares.notFoundHandler);
 app.use(middlewares.errorHandler);
