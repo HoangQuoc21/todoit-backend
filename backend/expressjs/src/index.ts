1abb211f4714b17466556ff29cf8a8288e37a513
Hoàng Quốc <hoangquoc2106@gmail.com>
2026-02-08 22:31:29 +0700

    Set up 3 handler cơ bản

--- backend/expressjs/src/index.ts
+++ backend/expressjs/src/index.ts
@@ -1,10 +1,16 @@
 import "dotenv/config";
 import express from "express";
+import { middlewares } from "./utils";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.get("/", middlewares.rootHandler);
+
+app.use(middlewares.notFoundHandler);
+app.use(middlewares.errorHandler);
 
 app.listen(PORT, () => {
   console.log(`--> Todoit server is running on http://localhost:${PORT}`);
---
+++ backend/expressjs/src/utils/middlewares.ts
@@ -0,0 +1,41 @@
+import type { RequestHandler, ErrorRequestHandler } from "express";
+import { status } from "http-status";
+import type { ApiResponse } from "../types";
+
+const rootHandler: RequestHandler = (req, res, next) => {
+  const response: ApiResponse<null> = {
+    success: true,
+    message: "Welcome to Todoit API",
+    data: null,
+  };
+
+  res.status(status.OK).json(response);
+};
+
+const notFoundHandler: RequestHandler = (req, res, next) => {
+  console.warn(`--> notFoundHandler: ${req.originalUrl} not found`);
+  const response: ApiResponse<null> = {
+    success: false,
+    message: "Route not found",
+    data: null,
+  };
+
+  res.status(status.NOT_FOUND).json(response);
+};
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error("--> errorHandler:", err.message);
+  const response: ApiResponse<null> = {
+    success: false,
+    message: "Internal Server Error",
+    data: null,
+  };
+
+  res.status(status.INTERNAL_SERVER_ERROR).json(response);
+};
+
+export const middlewares = {
+  rootHandler,
+  notFoundHandler,
+  errorHandler,
+};
